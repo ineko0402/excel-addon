29026f6348e816e0d96aac8b7c72cd8eae048587
ineko0402 <52097123+ineko0402@users.noreply.github.com>
2026-08-18 17:26:27 +0900

    GitHub Pagesによる自動公開を追加 (#2)
    GitHub Actionsで型検査、マニフェスト検証、Webpackビルド、Pages公開を自動化する。README、公開URL、日本語マニフェスト、入口ページも追加する。

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://ineko0402.github.io/excel-addon/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
@@ -51,6 +51,11 @@ module.exports = async (env, options) => {
       ],
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html",
+        inject: false,
+      }),
       new HtmlWebpackPlugin({
         filename: "taskpane.html",
         template: "./src/taskpane/taskpane.html",
